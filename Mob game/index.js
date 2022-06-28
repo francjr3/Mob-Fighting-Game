3dacf25673c1e2ec36d26ca3a9282c1e4ea17641
francjr3 <Francisco.s.rodriguez1@gmail.com>
2022-06-27 19:15:21 -0700

    Added animation loop and event listener
    Character has been added. WASD key presses are heard

--- Mob game/index.js
+++ Mob game/index.js
@@ -10,9 +10,43 @@ context.fillRect(0,0, canvas.width, canvas.height)
 
 const image = new Image()
 image.src = "./img/PSGmap.png"
+const playerImg = new Image()
+playerImg.src = "./img/playerDown.png"
+
 console.log(image)
 
 
-image.onload = () => {
-    context.drawImage(image, -650, -700)
+function animate() {
+    window.requestAnimationFrame(animate)
+    context.drawImage(image, -593, -800)
+    context.drawImage(
+        playerImg,
+        0,
+        0,
+        playerImg.width/4,
+        playerImg.height,
+        canvas.width/2 - playerImg.width/8, 
+        canvas.height/2 - playerImg.height/2,
+        playerImg.width/4,
+        playerImg.height
+    )
 }
+animate()
+
+window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case 'w':
+            console.log(e.key)
+            break
+        case 'a':
+            console.log(e.key)
+            break
+        case 's':
+            console.log(e.key)
+            break
+        case 'd':
+            console.log(e.key)
+            break
+
+    }
+})
