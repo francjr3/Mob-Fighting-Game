858bcbd06585cdd8bddf60351e9238cadda6fd69
francjr3 <Francisco.s.rodriguez1@gmail.com>
2022-06-28 20:25:41 -0700

    Modified volume levels

--- Mob game/data/audio.js
+++ Mob game/data/audio.js
@@ -3,36 +3,36 @@ const audio = {
     Map: new Howl({
         src: "./audio/map.wav",
         html5: true,
-        volume: 0.1
+        volume: 0.2
     }),
     initBattle: new Howl({
         src: "./audio/initBattle.wav",
         html5: true,
-        volume: 0.03
+        volume: 0.02
     }),
     battle: new Howl({
         src: "./audio/battle.mp3",
         html5: true,
-        volume: 0.05
+        volume: 0.07
     }),
     tackleHit: new Howl({
         src: "./audio/tackleHit.wav",
         html5: true,
-        volume: 0.1
-    }),
-    fireballHit: new Howl({
-        src: "./audio/fireballHit.wav",
-        html5: true,
-        volume: 0.1
+        volume: 0.08
     }),
     initFireball: new Howl({
         src: "./audio/initFireball.wav",
         html5: true,
-        volume: 0.1
+        volume: 0.08
+    }),
+    fireballHit: new Howl({
+        src: "./audio/fireballHit.wav",
+        html5: true,
+        volume: 0.07
     }),
     victory: new Howl({
         src: "./audio/victory.wav",
         html5: true,
-        volume: 0.2
+        volume: 0.3
     })
 }
